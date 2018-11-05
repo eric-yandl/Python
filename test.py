6138f62e15184096f86b80ba564d9fa4dd1a10cf
Eric <yanduanli2014@126.com>
2018-11-06 00:08:18 +0800

    20181105
    Signed-off-by: Eric <yanduanli2014@126.com>

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-print(100+1)
-pass
+import numpy as np
+import matplotlib.pyplot as plt
+
+func = np.poly1d(np.array([1,2,3,4]).astype(float))
+func1 = func.deriv(1)
+x = np.linspace(-10,10,30)
+y = func(x)
+y1 = func1(x)
+plt.plot(x,y,':r', label='y=f(x)')
+plt.plot(x,y1,'-g', label=r"y=f'(x)")
+plt.legend()
+plt.show()
